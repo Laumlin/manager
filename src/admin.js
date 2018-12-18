96c3193abc27dce5f037427be41f668253d8d054
郑康霖 <1255964530@qq.com>
2018-12-18 20:42:08 +0800

    主页：初始化主页的结构、布局和基础css样式

--- src/admin.js
+++ src/admin.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
 import { Row, Col } from 'antd';
 
 class Admin extends Component {
   render() {
     return (
-      <Row>
-        <Col span={3}>left</Col>
-        <Col span={21}>
+      <Row className="container">
+        <Col span={3} className="nav-left">
+          <NavLeft></NavLeft>
+        </Col>
+        <Col span={21} className="main">
           <Header></Header>
-          <Row>
+          <Row className="content">
             content
           </Row>
           <Footer></Footer>
