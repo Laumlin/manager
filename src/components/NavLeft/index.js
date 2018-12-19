8a32ef6d3181765f6f3d64e74c322873ad5bd24c
郑康霖 <1255964530@qq.com>
2018-12-19 16:24:06 +0800

    主页NavLeft组件：NavLeft组件的样式和初步使用antd Menu组件

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import nemuList from '../../config/menuConfig'
+import './index.less'
+import { Menu, Icon } from 'antd'
+const SubMenu = Menu.SubMenu
+
 
 class NavLeft extends Component {
+  componentDidMount() {
+    console.log(nemuList)
+  }
   render() {
     return (
-      <div>NavLeft</div>
+      <div>
+        <div>
+          <div className="logo">
+            <img src="/assets/logo.svg" alt="" />
+            <h1>Imooc MS</h1>
+          </div>
+          <Menu theme="dark">
+            <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+              <Menu.Item key="1">Option 1</Menu.Item>
+              <Menu.Item key="2">Option 2</Menu.Item>
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </div>
+      </div>
     )
   }
 }
