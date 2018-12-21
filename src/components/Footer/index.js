199c08ebeef6238db7c0252e00a6961aea89822c
郑康霖 <1255964530@qq.com>
2018-12-21 10:42:27 +0800

    主页Footer组件：完成Footer组件的内容和样式

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './index.less'
 
 class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <div className="footer">版权所有：laumlin（推荐使用chrome浏览器，可以获得更佳操作体验） 技术支持：laumlin</div>
     )
   }
 }
