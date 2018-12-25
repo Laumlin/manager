d60e6723d9f2c87fd365522c57c77ded1c2e7a0f
郑康霖 <1255964530@qq.com>
2018-12-25 16:54:55 +0800

    Form表单-Login组件：复杂登录表单的实现

--- src/pages/Form/Login.js
+++ src/pages/Form/Login.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, message, Icon, Checkbox } from 'antd'
 const FormItem = Form.Item
 
 class FormLogin extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const userInfo = this.props.form.getFieldsValue()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        message.success(`${userInfo.userName} 你的密码为：${userInfo.password}`)
+      } 
+    })
+  }
+
   render() {
+    const { getFieldDecorator } = this.props.form
     return (
       <div>
         <Card title='登录行内表单' style={{marginBottom: 10}}>
@@ -19,9 +31,64 @@ class FormLogin extends Component {
             </FormItem>
           </Form>
         </Card>
+        <Card title='登录垂直表单'>
+          <Form onSubmit={this.handleSubmit} style={{width: 300}}>
+            <FormItem>
+              {
+                getFieldDecorator('userName', {
+                  initialValue: '',
+                  rules: [
+                    { 
+                      required: true,
+                      message: '用户名不能为空！' 
+                    },
+                    {
+                      min:4, max: 10,
+                      message: '用户名在4-10！'
+                    },
+                    {
+                      pattern: /\w+/g,
+                      message: '用户名必须为字母或数字！'
+                    }
+                  ]
+                })(
+                  <Input prefix={<Icon type='user' />} placeholder='请输入用户名' />
+                )
+              }
+            </FormItem>
+            <FormItem>
+              {
+                getFieldDecorator('password', {
+                  rules: [
+                    {
+                      required: true,
+                      message: '密码不能为空！'
+                    }
+                  ]
+                })(
+                  <Input prefix={<Icon type='lock' />} type='password' placeholder='请输入密码' />
+                )
+              }    
+            </FormItem>
+            <FormItem>
+              {
+                getFieldDecorator('remember', {
+                  valuePropName: 'checked',
+                  initialValue: true
+                })(
+                  <Checkbox>记住密码</Checkbox>
+                )
+              } 
+              <a href='#' style={{float: 'right'}}>忘记密码</a>   
+            </FormItem>
+            <FormItem>
+              <Button type='primary' htmlType="submit">登录</Button>
+            </FormItem>
+          </Form>
+        </Card>
       </div>
     )
   }
 }
 
-export default FormLogin
+export default Form.create()(FormLogin)
