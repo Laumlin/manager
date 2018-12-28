6d9c406c3fb8ab536c144b57a615ef23debb06a8
郑康霖 <1255964530@qq.com>
2018-12-28 21:04:15 +0800

    Table-highTable: 表格操作

--- src/pages/Table/HighTable.js
+++ src/pages/Table/HighTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Table, Modal, message, Button } from 'antd'
+import { Card, Table, Modal, message, Button, Badge } from 'antd'
 import axios from './../../axios'
 
 class HighTable extends Component {
@@ -35,6 +35,18 @@ class HighTable extends Component {
   handleChange = (pagination, filters, sorter) => {
     this.setState({
       sorterInfo: sorter
+    })
+  }
+
+  handleDelete = (item) => {
+    let id = item.id
+    Modal.confirm({
+      title: '确认',
+      content: '您确定要删除此条数据吗？',
+      onOk: () => {
+        message.success('删除成功！')
+        this.getTableList()
+      }
     })
   }
 
@@ -326,6 +338,64 @@ class HighTable extends Component {
       }
     ]
 
+    const columns4 = [
+      {
+        title: '用户名',
+        dataIndex: 'username'
+      },
+      {
+        title: '性别',
+        dataIndex: 'sex',
+        render(sex) {
+          return sex === 1 ? '男' : '女'
+        }
+      },
+      {
+        title: '年龄',
+        dataIndex: 'age'
+      },
+      {
+        title: '状态',
+        dataIndex: 'state',
+        render(state) {
+          let config = {
+            1: <Badge status='success' text='咸鱼一条' />,
+            2: <Badge status='error' text='单身' />,
+            3: <Badge status='default' text='恋爱' />,
+            4: <Badge status='processing' text='分手' />,
+            5: <Badge status='warning' text='自由' />
+          }
+          return config[state]
+        }
+      },
+      {
+        title: '爱好',
+        dataIndex: 'interest',
+        render(interest) {
+          let config = {
+            1: '游泳',
+            2: '篮球',
+            3: '跑步',
+            4: '爬山',
+            5: '桌球',
+            6: '旅游',
+            7: '钓鱼'
+          } 
+          return config[interest]
+        }
+      },
+      {
+        title: '地址',
+        dataIndex: 'address'
+      },
+      {
+        title: '操作',
+        render: (text, item) => {
+          return <Button size='small' onClick={() => this.handleDelete(item)}>删除</Button>
+        }
+      }
+    ]
+
     return (
       <div>
         <Card title='头部固定'>
@@ -355,6 +425,15 @@ class HighTable extends Component {
             dataSource={this.state.dataSource}
             pagination={false}
             onChange={this.handleChange}
+          >
+          </Table>
+        </Card>
+        <Card title='操作按钮' style={{marginTop: 10}}>
+          <Table
+            bordered
+            columns={columns4}
+            dataSource={this.state.dataSource}
+            pagination={false}
           >
           </Table>
         </Card>
