9d30f0681bc67c68fff447900116e55185e190af
郑康霖 <1255964530@qq.com>
2019-01-07 22:42:07 +0800

    用户管理组件: 完成user组件的增加用户功能

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-import { Card, Button } from 'antd'
+import { Card, Button, Modal, Form, Input, Radio, DatePicker, Select } from 'antd'
 import axios from '../../axios'
 import utils from '../../utils/utils'
 import ETable from '../../components/ETable'
 import BaseForm from '../../components/BaseForm'
+import TextArea from 'antd/lib/input/TextArea';
+const RadioGroup = Radio.Group
+const FormItem = Form.Item
+const Option = Select.Option
 
 class User extends Component {
 
-  state = {}
+  state = {
+    isVisible: false
+  }
 
   params = {
     page: 1
@@ -49,8 +55,35 @@ class User extends Component {
     axios.requestList(this, '/user/list', this.params)
   }
 
+  // 员工操作
   handleOperate = (type) => {
-    
+    if (type === 'create') {
+      this.setState({
+        type,
+        isVisible:true,
+        title:'创建员工'
+      })
+    }
+  }
+
+  // 创建员工提交
+  handleSumbit = () => {
+    let type = this.state.type
+    let data = this.userForm.props.form.getFieldValue()
+    axios.ajax({
+      url: '/user/add',
+      data: {
+        params: data
+      }
+    }).then(res => {
+      if (res.code === 0) {
+        this.userForm.props.form.resetFields()
+        this.setState({
+          isVisible: false
+        })
+        this.requestList()
+      }
+    })
   }
 
   render() {
@@ -121,9 +154,92 @@ class User extends Component {
             pagination={this.state.pagination}
           />
         </div>
+        <Modal 
+          title={this.state.title}
+          visible={this.state.isVisible}
+          onOk={this.handleSumbit}
+          onCancel={() => {
+            this.userForm.props.form.resetFields()
+            this.setState({
+              isVisible: false
+            })
+          }}
+          width={600}
+        >
+          <UserForm 
+            type={this.state.type}
+            wrappedComponentRef={(form) => this.userForm=form}
+          />
+        </Modal>
       </div>
     )
   }
 }
+
+class UserForm extends Component {
+  render() {
+    const { getFieldDecorator } = this.props.form
+    const formItemLayout = {
+      labelCol: {
+        span: 5
+      },
+      wrapperCol: {
+        span: 19
+      }
+    }
+    return (
+      <div>
+        <Form>
+          <FormItem label='用户名' {...formItemLayout}>
+            {
+              getFieldDecorator('user_name')(
+                <Input type='text' placeholder='请输入用户名' />
+              )
+            }
+          </FormItem>
+          <FormItem label='性别' {...formItemLayout}>
+            {
+              getFieldDecorator('sex')(
+                <RadioGroup>
+                  <Radio value={1}>男</Radio>
+                  <Radio value={2}>女</Radio>
+                </RadioGroup>
+              )
+            }
+          </FormItem>
+          <FormItem label='状态' {...formItemLayout}>
+            {
+              getFieldDecorator('state')(
+                <Select>
+                  <Option value={1}>咸鱼</Option>
+                  <Option value={2}>程序猿</Option>
+                  <Option value={3}>射技师</Option>
+                  <Option value={4}>创业者</Option>
+                  <Option value={5}>风华浪子</Option>
+                </Select>
+              )
+            }
+          </FormItem>
+          <FormItem label='入职时间' {...formItemLayout}>
+            {
+              getFieldDecorator('user_date')(
+                <DatePicker />
+              )
+            }
+          </FormItem>
+          <FormItem label='地址' {...formItemLayout}>
+            {
+              getFieldDecorator('address')(
+                <TextArea rows={3} placeholder='请输入联系地址' />
+              )
+            }
+          </FormItem>
+        </Form>
+      </div>
+    )
+  }
+}
+
+UserForm = Form.create()(UserForm)
 
 export default User
