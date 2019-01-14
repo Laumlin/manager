c05455fd22a71be85b802ea4fc4329889f508e84
郑康霖 <1255964530@qq.com>
2019-01-14 14:15:40 +0800

    图标：饼图组件之二

--- src/pages/Echarts/pie/index.js
+++ src/pages/Echarts/pie/index.js
@@ -74,11 +74,72 @@ class Pie extends Component {
     return option
   }
 
+  getOption2 = () => {
+    let option = {
+      title: { 
+        text: '用户骑行订单',
+        x: 'center'
+      },
+      legend: {
+        orient: 'vertical',
+        right: 10,
+        top: 20,
+        bottom: 20,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a}<br />{b}:{c}({d}%)'
+      },
+      series: [
+        {
+          name: '订单量',
+          type: 'pie',
+          radius: ['50%', '80%'],
+          data: [ 
+            {
+              value: 1000,
+              name: '周一'
+            }, 
+            {
+              value: 2000,
+              name: '周二'
+            },
+            {
+              value: 1500,
+              name: '周三'
+            },
+            {
+              value: 3000,
+              name: '周四'
+            },
+            {
+              value: 2000,
+              name: '周五'
+            },
+            {
+              value: 1200,
+              name: '周六'
+            },
+            {
+              value: 800,
+              name: '周日'
+            }
+          ]
+        }
+      ]
+    }
+    return option
+  }
+
   render() {
     return (
       <div>
         <Card title='饼图标之一'>
           <ReactEcharts option={this.getOption()} theme='bike' style={{height: 500}} />
+        </Card>
+        <Card title='饼图标之二' style={{marginTop: 10}}>
+          <ReactEcharts option={this.getOption2()} theme='bike' style={{height: 500}} />
         </Card>
       </div>
     )
