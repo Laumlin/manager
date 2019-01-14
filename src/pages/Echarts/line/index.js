c771b7d8a143660d21b1346638e5b768ed60fd32
郑康霖 <1255964530@qq.com>
2019-01-14 14:48:11 +0800

    图标：折线图组件之二

--- src/pages/Echarts/line/index.js
+++ src/pages/Echarts/line/index.js
@@ -42,11 +42,48 @@ class Line extends Component {
     return option
   }
 
+  getOption2 = () => {
+    let option = {
+      title: { 
+        text: '用户骑行订单'
+      },
+      xAxis: {
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['ofo订单量', '摩拜订单量']
+      },
+      series: [
+        {
+          name: 'ofo订单量',
+          type: 'line',
+          data: [ 2000, 3000, 5500, 7000, 8000, 12000, 20000 ]
+        },
+        {
+          name: '摩拜订单量',
+          type: 'line',
+          data: [ 1500, 3000, 4500, 6000, 8000, 10000, 15000 ] 
+        },
+      ]
+    }
+    return option
+  }
+
+
   render() {
     return (
       <div>
         <Card title='折线图标之一'>
           <ReactEcharts option={this.getOption()} theme='bike' style={{height: 500}} />
+        </Card>
+        <Card title='折线图标之二' style={{marginTop: 10}}>
+          <ReactEcharts option={this.getOption2()} theme='bike' style={{height: 500}} />
         </Card>
       </div>
     )
