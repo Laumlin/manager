63dd2166870d0551639555beee11fd3a62e6f7c5
郑康霖 <1255964530@qq.com>
2019-01-17 14:18:43 +0800

    home主页：修改首页内容

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import './index.less'
 
 class Home extends Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <div className="home-wrapper">欢迎来到laumlin的后台管理系统</div>
